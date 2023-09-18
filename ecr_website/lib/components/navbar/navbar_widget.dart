0a3e7bcadc4e02b53e56562be0f0d6f255a03d98
El Código Recuerda <114629663+edunickdev@users.noreply.github.com>
2023-09-17 23:15:38 -0500

    update

--- ecr_website/lib/components/navbar/navbar_widget.dart
+++ ecr_website/lib/components/navbar/navbar_widget.dart
@@ -5,6 +5,8 @@ class Navbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final sizeWidth = MediaQuery.of(context).size.width;
+
     return Container(
       height: 100,
       width: double.infinity,
@@ -12,14 +14,41 @@ class Navbar extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Image.asset( "assets/images/logo-claro.png", height: 80, width: 160,),
+          SizedBox(
+              width: sizeWidth / 5,
+              child: Image.asset(
+                "assets/images/logo-claro.png",
+                height: 80,
+                width: 160,
+              )),
+          SizedBox(
+            width: sizeWidth / 5,
+            child: const Text.rich(
+              TextSpan(
+                text: "ECR",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
           const Text.rich(
             TextSpan(
-              text: "ECR",
+              text: "El Código",
               style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.brown),
+            ),
+          ),
+          Text.rich(
+            TextSpan(
+              text: "Recuerda",
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.yellow[400]),
             ),
           ),
         ],
